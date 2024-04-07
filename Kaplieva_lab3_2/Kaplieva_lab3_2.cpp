665009a5f28b851540c225e5f5de45dcd4efb0bb
Yaroslav <121808013+Mendeleev258@users.noreply.github.com>
2024-04-08 00:57:37 +0300

    Done

--- Kaplieva_lab3_2/Kaplieva_lab3_2.cpp
+++ Kaplieva_lab3_2/Kaplieva_lab3_2.cpp
@@ -12,36 +12,42 @@ void skip_token(std::ifstream& file, char& c);
 int read_word(std::ifstream& file, char& c, char* word);
 void create_matrix(std::ifstream& file, char matrix[][m], int len[], int& row);
 int check_file(std::ifstream& file);
-void print_word(char word[], int len, bool draft = false);
-void print_matrix(char matrix[][m], int len[], int row, const char* message, bool draft = false);
+void out_word(std::ofstream& file, char* word, int len);
+void out_matrix(std::ofstream& file, char matrix[][m], int len[], int row);
 
 // task
 
 bool is_vowel(char c);
 bool correct_word(char* word, int len);
+int compare(char word1[], int len1, char word2[], int len2);
+template <typename T>
+void my_swap(T& a, T& b);
+void swap_words(char* word1, int len1, char* word2, int len2);
+void simple_choice_sort(char matrix[][m], int len[], int row);
 
 
 int main()
 {
-	std::ifstream input("inp.txt");
+	std::ifstream input("C:/Users/jrosl/source/repos/Mendeleev258/Kaplieva_lab3_2/Kaplieva_lab3_2/inp.txt");
 	switch (check_file(input))
 	{
 	case -1:
-		std::cout << "File is not found\n";
+		std::cout << "File not found\n";
 		break;
 	case 0:
 		std::cout << "Empty file\n";
 		break;
 	default:
 		std::cout << "Correct file\n";
-		/*char matrix[35][15]; 
+		char matrix[35][15]; 
 		int len[35];
 		int row{};
 		bool draft = false;
 		create_matrix(input, matrix, len, row);
-		print_matrix(matrix, len, row, "");*/
-		
-		
+		simple_choice_sort(matrix, len, row);
+		std::ofstream output("C:/Users/jrosl/source/repos/Mendeleev258/Kaplieva_lab3_2/Kaplieva_lab3_2/out.txt");
+		out_matrix(output, matrix, len, row);
+		output.close();
 	}
 }
 
@@ -79,7 +85,8 @@ void create_matrix(std::ifstream& file, char matrix[][m], int len[], int& row)
 		if (!is_token(c))
 		{
 			len[row] = read_word(file, c, matrix[row]);
-			++row;
+			if (correct_word(matrix[row], len[row]))
+				row++;
 		}
 	}
 }
@@ -97,25 +104,19 @@ int check_file(std::ifstream& file)
 	return res;
 }
 
-void print_word(char word[], int len, bool draft)
+void out_word(std::ofstream& file, char* word, int len)
 {
-	if (draft)
-		std::cout << '=';
-	for (int i = 0; i < len; ++i)
-		std::cout << word[i];
-	std::cout << (draft ? "=" : "") << '\n';
+	for (int i{}; i < len; ++i)
+		file << word[i];
+	file << '\n';
 }
 
-void print_matrix(char matrix[][m], int len[], int row, const char* message, bool draft)
+void out_matrix(std::ofstream& file, char matrix[][m], int len[], int row)
 {
-	std::cout << message << '\n';
-	for (int i = 0; i < row; ++i)
-	{
-		print_word(matrix[i], len[i], draft);
-		if (draft)
-			std::cout << "---------------------------\n";
-	}
+	for (int i{}; i < row; ++i)
+		out_word(file, matrix[i], len[i]);
 }
+
 
 // task
 
@@ -139,6 +140,46 @@ bool correct_word(char* word, int len)
 		++i;
 	}
 	return res;
+}
+
+int compare(char word1[], int len1, char word2[], int len2)
+{
+	auto signL = [](int x) {return x == 0 ? 0 : x / abs(x); };
+	int len = len1 < len2 ? len1 : len2;
+	int result{}, i{};
+	while (i < len && word1[i] == word2[i])
+		++i;
+	if (i < len)
+		result = signL(word1[i] - word2[i]);
+	else
+		result = signL(len1 - len2);
+	return result;
+}
+
+template <typename T>
+void my_swap(T& a, T& b)
+{
+	T tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void swap_words(char* word1, int len1, char* word2, int len2)
+{
+	int max_len = (len1 > len2) ? len1 : len2;
+	for (int i = 0; i < max_len; ++i)
+		my_swap(word1[i], word2[i]);
+}
+
+void simple_choice_sort(char matrix[][m], int len[], int row)
+{
+	for (int i{}; i < row; ++i)
+		for (int j{}; j < row - i - 1; ++j)
+			if (compare(matrix[j], len[j], matrix[j + 1], len[j + 1]) > 0)
+			{
+				my_swap(len[j], len[j + 1]);
+				swap_words(matrix[j], len[j], matrix[j + 1], len[j + 1]);
+			}
 }
 
 
